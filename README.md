8352cd8f74dba5d4ed4686cae36a241b017d6b6d
Koorosh <mr.oplus@yahoo.fr>
2024-01-15 21:34:39 +0400

    verbose logging / buffered output

---
+++ README.md
@@ -0,0 +1,99 @@
+# Goshne
+
+Welcome to Goshne! Your new best friend in the quest for delicious deals. We dive into the depths of the [Snappfood](https://snappfood.ir/) website, wrestle with discounts (Food party and other parties), and deliver them straight to your Telegram (in your private messages or a Telegram channel of your choice). Perfect for all the foodie adventurers and bargain ninjas out there! Get ready to discover mouth-watering deals and share the joy with your network. Let the food hunt begin!
+
+## Installation
+
+Follow these steps to get Goshne up and running:
+
+### Clone the repository
+
+Start by cloning the Goshne repository to your local machine. You can do this by running the following command in your terminal:
+
+```bash
+git clone https://github.com/ahbanavi/goshne.git
+```
+
+### Copy and modify the configuration file
+
+Next, navigate to the `config` directory and make a copy of the `config.local.yaml.example` file. Rename the copy to `config.local.yaml`.
+
+```bash
+cd goshne/config
+cp config.local.yaml.example config.local.yaml
+```
+
+Open `config.local.yaml` in your favorite text editor and replace the placeholder values with your actual values. Refer to the [Configuration](#configuration) section for more details.
+
+### Run Goshne
+
+#### With Docker
+
+Navigate back to the root directory of the project and run the following command to start Goshne:
+
+```bash
+cd ..
+docker compose up -d # or `docker-compose up -d` for older versions
+```
+
+Goshne is now up and running with Docekr!
+
+P.S.: The only reason I instruct to clone the repository is to get the `config.local.yaml.example` and `docker-compose.yaml` files. If you prefer to not clone the repository, you can only download these two files and change the bind `volumes` in `docker-compose.yaml` to correct paths. The docker image is available on [Github Packages](https://github.com/ahbanavi/goshne/pkgs/container/goshne)
+
+#### With Python
+
+If you prefer to run Goshne without Docker, follow these steps:
+
+1. Navigate back to the root directory of the project:
+
+```bash
+cd ..
+```
+
+1. Create a virtual environment and activate it:
+
+```bash
+python3 -m venv venv
+source venv/bin/activate # For Linux and macOS, or use `venv\Scripts\activate.bat` for windows cmd and `venv\Scripts\Activate.ps1` for powershell.
+```
+
+1. Install the required Python packages:
+
+```bash
+pip3 install -r requirements.txt
+```
+
+1. Run Goshne:
+
+```bash
+python3 main.py
+```
+
+Goshne is now up and running, you can cancel it with `Ctrl+C` at any time.
+
+## Configuration
+
+The application's configuration is handled by `config.local.yaml.example`. To use it, copy the file and rename it to `config.local.yaml`.
+
+Here's a brief explanation of the configuration parameters:
+
+```yaml
+telegram:
+    token: token # Your Telegram bot token. This is required and should be a string.
+    endpoint: https://api.telegram.org/bot # The endpoint for Telegram bot API server. This is optional and defaults to https://api.telegram.org/bot. Your bot token will be appended to this.
+
+schedule:
+    mins: 15 # The interval in minutes at which the bot will check for discounts.
+
+peoples: # At least one person is required.
+    person_name: # The name of a person to send deals to. It can be anything you want and doesn't have any effect on the bot's functionality, but should be unique and string.
+        chat_id: chat_id # The telegram chat id that the bot will send deals to. This is required and should be integer, you can find it with the help of https://t.me/username_to_id_bot.
+        lat: lat # The latitude for the person's location. This is required.
+        long: long # The longitude for the person's location. This is required.
+        threshold: 30 # The discount threshold. This is optional and defaults to 0. If set, the bot will only send deals with a discount greater than or equal to this value.
+
+
+    # More people can be added here in the same format as above.
+```
+
+Replace `token`, `person_name`, `chat_id`, `lat`, `long`, and `threshold` with your actual values.
---
+++ main.py
@@ -0,0 +1,200 @@
+import hashlib
+import json
+import random
+import sys
+import time
+from datetime import datetime, timedelta
+
+import pytz
+import requests
+import schedule
+import yaml
+from sqlitedict import SqliteDict
+
+local_tz = pytz.timezone("Asia/Tehran")
+
+TOMAN_FORMATTER = "{:,}"
+TEST = len(sys.argv) > 1 and sys.argv[1] == "-t"
+
+# load emojist from resource/food-emojis.json
+with open("resource/food-emojis.json", encoding="UTF-8") as f:
+    FOOD_EMOJIS = json.load(f)
+
+HEADERS = {
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "DNT": "1",
+    "Origin": "https://snappfood.ir",
+    "Referer": "https://snappfood.ir",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "TE": "trailers",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",  # noqa
+}
+
+# read config from yaml
+try:
+    with open("config/config.local.yaml", "r", encoding="UTF-8") as f:
+        CONFIG = yaml.load(f, Loader=yaml.FullLoader)
+except FileNotFoundError:
+    print("❗️ ERR: config.local.yaml not found")
+    sys.exit(1)
+
+db = SqliteDict("storage/db.sqlite")
+
+
+def get_and_send(name, lat, long, chat_id, threshold=0):
+    """get data from snapp based on inputs and send to telegram
+
+    Args:
+        name (string): name of person, used for creating hash
+        lat (string): latitude
+        long (string): longitude
+        chat_id (int): telegram chat id to send to
+        threshold (int, optional): threshold for getting discounts, default is 0
+    """
+    home_url = f"https://snappfood.ir/search/api/v1/desktop/new-home?lat={lat}&long={long}&optionalClient=WEBSITE&client=WEBSITE&deviceType=WEBSITE&appVersion=8.1.1&locale=fa"
+
+    # get home page
+    HEADERS["Host"] = "snappfood.ir"
+    print(f"🕒 Downloading Offers for {name}")
+    home_page = requests.get(home_url, headers=HEADERS,timeout=CONFIG['timeout'])
+    if home_page.status_code != 200:
+        print(f"❗️ ERR: {home_page.status_code}")
+        if home_page.status_code == 403:
+            print(f"❗️ ERR: Our IP Address is blocked by Snappfood, please change your IP Address and try again")
+            exit(254)
+        return False
+    home_data = home_page.json()
+    if "error" in home_data:
+        print(f"❗️ ERR: {home_data['error']}")
+        return False
+
+    if home_data["data"]["result"][1]["id"] != 8:
+        return False
+
+    # get party_url
+    party_url = home_data["data"]["result"][1]["data"]["url"]
+
+    # override Host header based on party_url
+    HEADERS["Host"] = party_url.split("/")[2]
+    print(f"🕒 Extracting Party Foods")
+    response = requests.get(party_url, headers=HEADERS,timeout=CONFIG['timeout']).json()
+    if "error" in response:
+        print(f"❗️ ERR: {response['error']}")
+        return False
+
+    party_title = response["data"]["title"]
+    # extract hashtag, replace all none words with _
+    party_hashtag = "#" + "\_".join(party_title.split())
+
+    products = response["data"]["products"]
+
+    for product in products:
+        if product["discountRatio"] >= threshold:
+            discount_price = product["price"] * (100 - product["discountRatio"]) / 100
+
+            product_hash = hashlib.md5(
+                name.encode("utf-8")
+                + product["title"].encode("utf-8")
+                + str(discount_price).encode("utf-8")
+                + product["vendorTitle"].encode("utf-8")
+            ).hexdigest()
+
+            if not TEST and product_hash in db:
+                if datetime.now(local_tz) - db[product_hash]["time"] < timedelta(hours=12):  # fmt: skip
+                    continue
+                else:
+                    db[product_hash] = {
+                        "time": datetime.now(local_tz),
+                    }
+            else:
+                db[product_hash] = {
+                    "time": datetime.now(local_tz),
+                }
+
+            vendor_url = "https://snappfood.ir/restaurant/menu/" + product["vendorCode"]
+            # fmt: off
+            out = random.choice(FOOD_EMOJIS) + " " + party_hashtag + " [" + product["title"] + "](" + vendor_url+ ")\n" # noqa
+            out += "🍽 " + product["vendorTypeTitle"] + " " + product["vendorTitle"] + "\n"
+            out += "🛍 ‏*" + str(product["discountRatio"]) + "%*\n"
+            out += "💵 *" + TOMAN_FORMATTER.format(product["price"]) + "* ت\n"
+            out += "💸 *" + TOMAN_FORMATTER.format(int(discount_price)) + "* ت (" + TOMAN_FORMATTER.format(int(product["price"] - discount_price)) + "-)\n" # noqa
+            out += "🛵 *" + TOMAN_FORMATTER.format(int(product["deliveryFee"])) + "* ت\n"
+            out += "⭐️ " + str(round(product["rating"], 2)) + " از " + str(product["vote_count"]) + " رای \n"
+            out += "⌛ ‏" + str(product["remaining"]) + "\n"
+            # fmt: on
+            print(f"📤 Sending {product['title']} to {name}")
+            requests.post(
+                CONFIG["telegram"].get("endpoint", "https://api.telegram.org/bot")
+                + CONFIG["telegram"]["token"]
+                + "/sendPhoto",
+                data={
+                    "chat_id": chat_id,
+                    "photo": product["main_image"]
+                    or "https://raw.githubusercontent.com/ahbanavi/goshne/main/resource/default.jpg",  # default image
+                    "caption": out,
+                    "parse_mode": "Markdown",
+                    # add inline button
+                    "reply_markup": json.dumps(
+                        {
+                            "inline_keyboard": [
+                                [
+                                    {
+                                        "text": "🛒 خرید",
+                                        "url": vendor_url,
+                                    }
+                                ]
+                            ]
+                        }
+                    ),
+                },
+                timeout=CONFIG['timeout']
+            )
+
+
+# for each person in config peoples get_and_send
+def main():
+    try:
+        for person_name in CONFIG["peoples"]:
+            person = CONFIG["peoples"][person_name]
+            get_and_send(
+                name=person_name,
+                lat=person["lat"],
+                long=person["long"],
+                chat_id=person["chat_id"],
+                threshold=person.get("threshold", 0),
+            )
+
+            if TEST:
+                break
+
+        # store db
+        db.commit()
+    except Exception as ex:
+        print(f"❗️ ERR: {ex}")
+        return False
+
+
+if TEST:
+    main()
+
+else:
+    print(f"Running app every {CONFIG['schedule']['mins']} minutes... with Http Timeout of {CONFIG['timeout']} seconds")
+    schedule.every(CONFIG["schedule"]["mins"]).minutes.do(main)
+    main()
+    while 1:
+        n = schedule.idle_seconds()
+        if n is None:
+            # no more jobs
+            break
+        elif n > 0:
+            # sleep exactly the right amount of time
+            time.sleep(n)
+        schedule.run_pending()
+
+db.close()
